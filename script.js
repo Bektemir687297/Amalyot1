18ad789139be6c7dc9e94e9689925b21df288788
Bek <97296396+Bektemir687297@users.noreply.github.com>
2022-02-21 16:58:53 +0500

    Bek

--- script.js
+++ script.js
@@ -13,7 +13,23 @@ const personalNewsDB = {
 for (let i = 0; i < 2; i++) {
   const a = prompt("Ohirgai ko'rgan yangiliklaringizdan biri?"),
     b = prompt("Unga qancha boho bergan bo'lardingiz?");
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalNewsDB.news[a] = b;
+    console.log("tayor");
+  } else {
+    console.log("Error");
+    i--;
+  }
 }
 
-personalNewsDB.news[a] = b;
+if (personalNewsDB.count < 10) {
+  console.log("Judda kam sonli yangiliklar o'qilibdi");
+} else if (personalNewsDB.count >= 10 && personalNewsDB.count < 30) {
+  console.log("Siz klassik tamoshabinsiz");
+} else if (personalNewsDB.count >= 30) {
+  console.log("Siz yangiliklar ishqibozi ekansiz");
+} else {
+  console.log("Xato yuz berdi");
+}
+
 console.log(personalNewsDB);
