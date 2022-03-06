aba6e411e1ee2d8501e521304603d56c0a3e5d70
Bek <97296396+Bektemir687297@users.noreply.github.com>
2022-03-06 20:22:55 +0500

    bek

--- script.js
+++ script.js
@@ -1,67 +1,85 @@
 " use strict";
 
-let numberOfNews;
-
-function startProject() {
-  numberOfNews = +prompt("Siz qancha yangliki ko'rdingiz?");
-
-  while (numberOfNews == "" || numberOfNews == null || isNaN(numberOfNews)) {
-    numberOfNews = +prompt("Siz qancha yangliki ko'rdingiz?");
-  }
-}
-startProject();
-
 const personalNewsDB = {
-  count: numberOfNews,
+  count: 0,
   news: {},
   actors: {},
   genres: [],
   privat: false,
-};
+  startProject: function () {
+    personalNewsDB.count = +prompt("Siz qancha yangliki ko'rdingiz?");
 
-function rememberNews() {
-  for (let i = 0; i < 2; i++) {
-    const a = prompt("Ohirgai ko'rgan yangiliklaringizdan biri?"),
-      b = prompt("Unga qancha boho bergan bo'lardingiz?");
-    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-      personalNewsDB.news[a] = b;
-      console.log("tayor");
-    } else {
-      console.log("Error");
-      i--;
+    while (
+      personalNewsDB.count == "" ||
+      personalNewsDB.count == null ||
+      isNaN(personalNewsDB.count)
+    ) {
+      personalNewsDB.count = +prompt("Siz qancha yangliki ko'rdingiz?");
     }
-  }
-}
+  },
+  rememberNews: function () {
+    for (let i = 0; i < 2; i++) {
+      const a = prompt("Ohirgai ko'rgan yangiliklaringizdan biri?"),
+        b = prompt("Unga qancha boho bergan bo'lardingiz?");
+      if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalNewsDB.news[a] = b;
+        console.log("tayor");
+      } else {
+        console.log("Error");
+        i--;
+      }
+    }
+  },
+  showPesonalLevel: function () {
+    if (personalNewsDB.count < 10) {
+      console.log("Judda kam sonli yangiliklar o'qilibdi");
+    } else if (personalNewsDB.count >= 10 && personalNewsDB.count < 30) {
+      console.log("Siz klassik tamoshabinsiz");
+    } else if (personalNewsDB.count >= 30) {
+      console.log("Siz yangiliklar ishqibozi ekansiz");
+    } else {
+      console.log("Xato yuz berdi");
+    }
+  },
+  showMyDB: function (hidden) {
+    if (!hidden) {
+      console.log(personalNewsDB);
+    }
+  },
+  toggleVisibleMyDB: function () {
+    if (personalNewsDB.privat) {
+      personalNewsDB.privat = false;
+    } else {
+      personalNewsDB.privat = true;
+    }
+  },
+  writeYourGenres: function () {
+    /*for (let i = 1; i < 2; i++) {
+      let genre = prompt(`Sizning sevimli janringiz: no'mir ${i}`);
+      if (genre === "" || genre === null) {
+        console.log("Siz noto'g'ri malumot kiritingiz");
+        i--;
+      } else {
+        personalNewsDB.genres[i - 1] = genre;
+      }
+    }*/
 
-rememberNews();
+    for (let i = 1; i < 2; i++) {
+      let genre = prompt(
+        `Sizning sevimli janringizni, vergul bilan ajratib yozingi`
+      ).toLowerCase();
 
-function showPesonalLevel() {
-  if (personalNewsDB.count < 10) {
-    console.log("Judda kam sonli yangiliklar o'qilibdi");
-  } else if (personalNewsDB.count >= 10 && personalNewsDB.count < 30) {
-    console.log("Siz klassik tamoshabinsiz");
-  } else if (personalNewsDB.count >= 30) {
-    console.log("Siz yangiliklar ishqibozi ekansiz");
-  } else {
-    console.log("Xato yuz berdi");
-  }
-}
+      if (genre === "" || genre === null) {
+        console.log("siz noto'g'ri malumot kiritingiz");
+        i--;
+      } else {
+        personalNewsDB.genres = genre.split(", ");
+        personalNewsDB.genres.sort();
+      }
+    }
 
-showPesonalLevel();
-
-function showMyDB(hidden) {
-  if (!hidden) {
-    console.log(personalNewsDB);
-  }
-}
-
-showMyDB(personalNewsDB.privat);
-
-function writeYourGenres() {
-  for (let i = 1; i <= 3; i++) {
-    personalNewsDB.genres[i - 1] = prompt(
-      `Sizning sevimli janringiz no'miri ${i}`
-    );
-  }
-}
-writeYourGenres();
+    personalNewsDB.genres.forEach((item, i) => {
+      console.log(`Sizning sevimli janringiz: no'mir ${i + 1} bu ${item}`);
+    });
+  },
+};
